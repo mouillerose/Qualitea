81abd31db171a940c189f705b2df91c5da44faef
mouillerose <79447929+mouillerose@users.noreply.github.com>
2021-04-03 22:58:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QualiTea - Accueil</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+
+<?php include 'php/header_index.php'; ?>
+<?php include 'php/side_menu_index.php'; ?>
+
+<section id="milieu">
+        <img src="img/image_accueil.png" alt="Rien de mieux qu'un thé de qualité" id="image_accueil">
+</section>
+
+<?php include 'php/footer.php'; ?>
+
+</body>
+</html>
